cedc6c3d0b01f33ef6913b008ff49a23f2efeed6
Nadya <nadezhda.tarasova.dmi@gmail.com>
2025-06-24 14:31:50 +0300

    Удалила hw02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,12 @@ public class Main {
         System.out.printf("Hello World!");
         System.out.println("Hello World!"); // This line is highlighted as a warning
         System.out.println("Hello HW02!"); // This line is highlighted as a warning
-
+    // Delete hw02
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello HW03!"); // This line is highlighted as a warning
+        System.out.println("Hello HW04!"); // This line is highlighted as a warning
+        System.out.println("Hello HW05!"); // This line is highlighted as a warning
+        System.out.println("Hello HW06!"); // This line is highlighted as a warning
     }
 }
